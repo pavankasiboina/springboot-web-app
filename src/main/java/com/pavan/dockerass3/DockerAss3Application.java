20502dd33060b4353633141d959258560cc631c3
pavan <pavankasiboina@gmail.com>
2023-04-25 11:07:19 +0530

    Spring-boot sample-demo

---
+++ src/main/java/com/pavan/dockerass3/DockerAss3Application.java
@@ -0,0 +1,13 @@
+package com.pavan.dockerass3;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DockerAss3Application {
+
+	public static void main(String[] args) {
+		SpringApplication.run(DockerAss3Application.class, args);
+	}
+
+}
---
+++ src/test/java/com/pavan/dockerass3/DockerAss3ApplicationTests.java
@@ -0,0 +1,13 @@
+package com.pavan.dockerass3;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class DockerAss3ApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
